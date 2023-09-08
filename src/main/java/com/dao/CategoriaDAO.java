f917d4372470851cb6643cc0360210f92533b5e0
Bohorquez <jaelbkz@icloud.com>
2023-09-08 16:04:52 -0600

    adicion con inner join

--- src/main/java/com/dao/CategoriaDAO.java
+++ src/main/java/com/dao/CategoriaDAO.java
@@ -12,6 +12,7 @@ import com.alura.jdbc.modelo.Categoria;
 public class CategoriaDAO {
 
     private Connection con;
+
     public CategoriaDAO(Connection con) {
         this.con = con;
     }
@@ -22,17 +23,17 @@ public class CategoriaDAO {
             var querySelect = "SELECT ID, NOMBRE FROM CATEGORIAS";
             System.out.println(querySelect);
             final PreparedStatement decla = con.prepareStatement(querySelect);
-            try(decla){
-            final ResultSet resu = decla.executeQuery();
-            try(resu){
-                while (resu.next()) {
-                    var categoria = new Categoria(resu.getInt("ID"),
-                    resu.getString("NOMBRE"));
-                    
-                    result.add(categoria);
+            try (decla) {
+                final ResultSet resu = decla.executeQuery();
+                try (resu) {
+                    while (resu.next()) {
+                        var categoria = new Categoria(resu.getInt("ID"),
+                                resu.getString("NOMBRE"));
+
+                        result.add(categoria);
+                    }
                 }
             }
-        }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -41,26 +42,31 @@ public class CategoriaDAO {
 
     public List<Categoria> listarConProductos() {
 
- List<Categoria> result = new ArrayList<>();
+        List<Categoria> result = new ArrayList<>();
         try {
             var querySelect = "SELECT C.ID, P.NOMBRE, P.DESCRIPCION, P.CANTIDAD ,C.NOMBRE FROM CATEGORIAS C INNER JOIN PRODUCTO P ON C.ID = P.CATEGORIA_ID";
             System.out.println(querySelect);
             final PreparedStatement decla = con.prepareStatement(querySelect);
-            try(decla){
-            final ResultSet resu = decla.executeQuery();
-            try(resu){
-                while (resu.next()) {
-                    var categoria = new Categoria(resu.getInt("ID"),
-                    resu.getString("NOMBRE"));
-                    
-                    result.add(categoria);
+            try (decla) {
+                final ResultSet resu = decla.executeQuery();
+                try (resu) {
+                    while (resu.next()) {
+                        Integer id = resu.getInt("ID");
+                        String nombre = resu.getString("NOMBRE");
+                        var categoria = result.stream().filter(cat -> cat.getId()).equals(categoriaId).finAny()
+                                .orElseGet(() -> {
+                                });
+
+                        new Categoria(id, nombre);
+
+                        result.add(categoria);
+                    }
                 }
             }
-        }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return result;   
+        return result;
     }
-    
+
 }
